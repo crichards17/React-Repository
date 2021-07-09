d1538d37e92020a17a0db7c98c1dc0a683dc1ac7
Cameron Richards <crichards1717@gmail.com>
2021-07-08 23:53:05 -0700

    Updates

---
+++ precache-manifest.81aa415add41cfbfcaf94f4fbee8cdc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cbfd373895885ccdc8204846beab10",
+    "url": "/crichards17/React-Repository/index.html"
+  },
+  {
+    "revision": "58f52de69499f7fed86b",
+    "url": "/crichards17/React-Repository/static/css/2.cd554934.chunk.css"
+  },
+  {
+    "revision": "6d1585e8d096ec360a4c",
+    "url": "/crichards17/React-Repository/static/css/main.9db21343.chunk.css"
+  },
+  {
+    "revision": "58f52de69499f7fed86b",
+    "url": "/crichards17/React-Repository/static/js/2.c8ffa165.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/crichards17/React-Repository/static/js/2.c8ffa165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1585e8d096ec360a4c",
+    "url": "/crichards17/React-Repository/static/js/main.d0ce0224.chunk.js"
+  },
+  {
+    "revision": "8c8f9b70a062cdf2bba8",
+    "url": "/crichards17/React-Repository/static/js/runtime-main.c9a64c62.js"
+  }
+]);
