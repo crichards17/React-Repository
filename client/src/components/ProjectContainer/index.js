1652176abcfec34413254fe58aa5e8f94ef4cad7
Cameron Richards <crichards1717@gmail.com>
2021-07-09 00:01:01 -0700

    updating project list, deployed to GitHub pages

--- client/src/components/ProjectContainer/index.js
+++ client/src/components/ProjectContainer/index.js
@@ -16,11 +16,11 @@ function ProjectContainer() {
                 </div>
                 <div className="col-sm my-2">
                     <ProjectCard 
-                        title="React Employee Repository"
-                        image="https://raw.githubusercontent.com/crichards17/React-Employee-Repository/main/Assets/Directory_Demo.gif"
-                        description="A sample sortable and searchable directory application built using React and MongoDB via Mongoose"
-                        github="https://github.com/crichards17/React-Employee-Repository"
-                        deployed="https://crichards17.github.io/React-Employee-Repository//"
+                        title="Google Books Search"
+                        image="https://github.com/crichards17/Google-Books-Search/blob/main/assets/images/Sample.PNG?raw=true"
+                        description="An application allowing a user to search the Google Books library for titles, review details and external links for those titles, and manage a Saved list."
+                        github="https://github.com/crichards17/Google-Books-Search"
+                        deployed="https://google-book-search-cr.herokuapp.com/"
                     />
                 </div>
             </div>
@@ -41,6 +41,26 @@ function ProjectContainer() {
                         description="A sample sortable and searchable directory application built using React and MongoDB via Mongoose"
                         github="https://github.com/crichards17/React-Employee-Repository"
                         deployed="https://crichards17.github.io/React-Employee-Repository//"
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm my-2">
+                    <ProjectCard 
+                        title="Fitness Tracker"
+                        image="https://raw.githubusercontent.com/crichards17/Tech-Blog/main/assets/sample.PNG"
+                        description="A workout-tracking application demonstrating usage of Express.js with a MongoDB back end via Mongoose."
+                        github="https://github.com/crichards17/Fitness-tracker"
+                        deployed="https://fitness-tracker-cr.herokuapp.com/?id=60c88fb69ee6ca0015c76606"
+                    />
+                </div>
+                <div className="col-sm my-2">
+                    <ProjectCard 
+                        title="Toolshare"
+                        image="https://raw.githubusercontent.com/crichards17/React-Employee-Repository/main/Assets/Directory_Demo.gif"
+                        description="A site for sharing, trading, and borrowing tools: a community tradition, brought to the web."
+                        github="https://github.com/crichards17/Toolshare"
+                        deployed="https://pacific-tor-20392.herokuapp.com/"
                     />
                 </div>
             </div>
